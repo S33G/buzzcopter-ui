756dc0b0f0ecc7f4a35888e74e9316a207376bcf
seeg <seeg@dev.dev.local>
2024-04-05 12:11:17 +0100

    UI refinement

--- src/components/EventLog.tsx
+++ src/components/EventLog.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IEventLogProps } from '../types';
+import '../assets/eventlog.scss'
 
 const getEventDateTime = (time: string) => {
   const dateTime = new Date(time);
@@ -13,28 +14,32 @@ const getEventDateTime = (time: string) => {
     formattedTime = ` @ ${dateTime.getHours()}:${dateTime.getMinutes()}:${dateTime.getSeconds()}`;
   }
 
-
   return `${dayNames[dateTime.getDay()]} ${dateTime.getDate()} ${monthNames[dateTime.getMonth()]} ${dateTime.getFullYear()} ${formattedTime}`;
 }
 
 const EventLog: React.FC<IEventLogProps> = ({events}) => {
 
-  return <>
-  <h1>Event Log</h1>
-    {events.map((event, index) => (
-      <article key={index}>
-        <div style={{ display: 'flex', justifyContent: 'space-between', margin: '1rem 0' }}>
-        <h2 style={{ fontSize: '1.5rem', margin: '0', textAlign: 'left'}}>{event.event}</h2>
-          <div style={{ border: '1px solid black', padding: '.25rem', borderRadius: '1rem', width: '2rem', height: '2rem', textAlign: 'center' }}>{index + 1}</div>
-        </div>
+  // Do the same as above but make it look like a table
+  return (
+    <div className="event-log">
+      <h1>Event Log</h1>
+      {events.map((event, index) => (
+        <article key={index}>
+          <i>{index + 1}</i>
+          <div>
+            <time>{getEventDateTime(event.time)}</time>
+          </div>
+          <div>
+            <h2>{event.event}</h2>
+          </div>
+          <div>
+            <p>{event.description ? event.description : '-'}</p>
+          </div>
 
-        <p>{event.description}</p>
-        <time style={{ fontSize: '.5rem', margin: '0' }}>{getEventDateTime(event.time)}</time>
-
-        <hr />
-      </article>
-    ))}
-  </>
+        </article>
+      ))}
+    </div>
+  )
 }
 
 
